36ecd79b00578075272d0ed768656519b16366f9
Sakunit Iamenetre <waterfallsist1@gmail.com>
2023-04-21 16:55:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,26 +15,7 @@ def predict():
     #print(estimated_sales)
     new_estimated_sales = (str(estimated_sales).lstrip('[').rstrip(']'))
     positive5,negative5 = coding.ShapCalculate(feainput,new_estimated_sales)
-    if int(new_estimated_sales) == 0:
-        text_estimated_sales = '0-20000'
-    elif int(new_estimated_sales) == 1:
-        text_estimated_sales = '20000-50000'
-    elif int(new_estimated_sales) == 2:
-        text_estimated_sales = '50000-100000'
-    elif int(new_estimated_sales) == 3:
-        text_estimated_sales = '100000-200000'
-    elif int(new_estimated_sales) == 4:
-        text_estimated_sales = '200000-500000'
-    elif int(new_estimated_sales) == 5:
-        text_estimated_sales = '500000-1000000'
-    elif int(new_estimated_sales) == 6:
-        text_estimated_sales = '1000000-2000000'
-    elif int(new_estimated_sales) == 7:
-        text_estimated_sales = '2000000-5000000'
-    elif int(new_estimated_sales) == 8:
-        text_estimated_sales = '5000000-10000000'
-    elif int(new_estimated_sales) == 9:
-        text_estimated_sales = '10000000-200000000'
+    text_estimated_sales = coding.classdefine(new_estimated_sales)
     return jsonify({'estimated sales class': new_estimated_sales,
                     'estimated sales': text_estimated_sales,
                     'positive1': positive5[0],
@@ -60,26 +41,7 @@ def predictno_ratings():
     #print(estimated_sales)
     new_estimated_sales = (str(estimated_sales).lstrip('[').rstrip(']'))
     positive5,negative5 = coding.ShapCalculateNoratings(feainput,new_estimated_sales)
-    if int(new_estimated_sales) == 0:
-        text_estimated_sales = '0-20000'
-    elif int(new_estimated_sales) == 1:
-        text_estimated_sales = '20000-50000'
-    elif int(new_estimated_sales) == 2:
-        text_estimated_sales = '50000-100000'
-    elif int(new_estimated_sales) == 3:
-        text_estimated_sales = '100000-200000'
-    elif int(new_estimated_sales) == 4:
-        text_estimated_sales = '200000-500000'
-    elif int(new_estimated_sales) == 5:
-        text_estimated_sales = '500000-1000000'
-    elif int(new_estimated_sales) == 6:
-        text_estimated_sales = '1000000-2000000'
-    elif int(new_estimated_sales) == 7:
-        text_estimated_sales = '2000000-5000000'
-    elif int(new_estimated_sales) == 8:
-        text_estimated_sales = '5000000-10000000'
-    elif int(new_estimated_sales) == 9:
-        text_estimated_sales = '10000000-200000000'    
+    text_estimated_sales = coding.classdefine(new_estimated_sales)
     return jsonify({'estimated sales class ': new_estimated_sales,
                     'estimated sales': text_estimated_sales,
                     'positive1': positive5[0],
